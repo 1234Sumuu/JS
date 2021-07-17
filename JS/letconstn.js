2b390f6c84c5c1f541104ecb99c56f9c70796238
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2021-07-17 08:27:44 -0700

    Add files via upload

---
+++ JS/letconstn.js
@@ -0,0 +1,17 @@
+const hubby = 'Sodor Uddin';
+console.log(hubby);
+
+const numbers =[12, 18];
+numbers[1] = 88;
+numbers.push(11);
+const nayok = {name: "Sakib Khan" ,phone=6457};
+console.log(numbers);
+
+let patientName = "Kajla didi";
+patientName = "Nilufer Khala";
+console.log(paitentName);
+
+for(let i =0; i<10; i++){
+    sum = sum+1;
+}
+console.log(i);
